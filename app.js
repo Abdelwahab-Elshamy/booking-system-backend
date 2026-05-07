a191f1b231e7ce0185b5780c16dfc4c5efa2889c
Abdelwahab <aa5070338@gmail.com>
2026-05-07 03:27:17 +0300

    fix cors for production

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://booking-system-frontend-rust.vercel.app/login",
+    ],
   }),
 );
 
